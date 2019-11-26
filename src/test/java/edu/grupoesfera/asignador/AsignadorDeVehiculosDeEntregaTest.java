b5c502d9b2ea6dc693b379d8ab01bca7f5210489
Simonetti <ASimonetti@corp.ute.com.uy>
2019-11-26 14:14:44 -0300

    primera subida

---
+++ src/test/java/edu/grupoesfera/asignador/AsignadorDeVehiculosDeEntregaTest.java
@@ -0,0 +1,56 @@
+package edu.grupoesfera.asignador;
+
+import org.junit.Test;
+
+import edu.grupoesfera.modelo.Envio;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+
+
+public class AsignadorDeVehiculosDeEntregaTest {
+
+	@Test
+	public void cincoPaquetes() {
+		
+		List<String> paquetes = new 	LinkedList <>();
+		paquetes.add("paquetes");
+		paquetes.add("perro");
+		
+		AsignadorVehiculos asignador = new AsignadorVehiculos();
+		Envio envio = asignador.asignar(paquetes);
+		assertThat(envio.getVehiculo()).isEqualTo("BICICLETA");
+		
+	}
+	
+	@Test
+	public void cincoADiezPaquetes() {
+		
+		List<String> paquetes = new 	LinkedList <>();
+		for(int i=0; i<10; i++)
+			paquetes.add("paquetes");
+		
+		AsignadorVehiculos asignador = new AsignadorVehiculos();
+		Envio envio = asignador.asignar(paquetes);
+		assertThat(envio.getVehiculo()).isEqualTo("MOTO");
+		
+	}
+	
+	@Test
+	public void diezATreintaPaquetes() {
+		
+		List<String> paquetes = new 	LinkedList <>();
+		for(int i=0; i<30; i++)
+			paquetes.add("paquetes");
+		
+		AsignadorVehiculos asignador = new AsignadorVehiculos();
+		Envio envio = asignador.asignar(paquetes);
+		assertThat(envio.getVehiculo()).isEqualTo("AUTO");
+		
+	}
+}
---
+++ src/test/java/edu/grupoesfera/asignador/AsignadorVehiculos.java
@@ -0,0 +1,23 @@
+package edu.grupoesfera.asignador;
+
+import java.util.List;
+
+import edu.grupoesfera.modelo.Envio;
+import net.bytebuddy.implementation.bytecode.Throw;
+
+
+public class AsignadorVehiculos {
+	public Envio asignar(List<String> paquetes) {
+		
+		Envio envio = new Envio();
+		if(paquetes.size() < 5)
+			envio.setVehiculo("BICICLETA");
+		else if(paquetes.size() < 10)
+			envio.setVehiculo("MOTO");
+		else if(paquetes.size() < 30)
+			envio.setVehiculo("AUTO");
+		else {
+		}
+		return envio;
+	}
+}
